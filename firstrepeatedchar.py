06656967e015f16006b699cf2fd31f11cbdf296e
Ratnadeep Kashinath Waghmare <ratnadeepwaghmare@gmail.com>
2025-03-29 11:46:37 +0530

    firstrepeatedchar.py

--- firstrepeatedchar.py
+++ firstrepeatedchar.py
@@ -5,6 +5,5 @@ flag_1 = 0
 for i,v in enumerate(x.split()):
     if v==x.split()[i+1]:
         flag_1 += 1
-        if flag_1 ==2:
-            print("Second Most Repeated Word is==>",v)
-            break
+        print("Second Most Repeated Word is==>",v)
+        break
